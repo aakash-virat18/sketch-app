df2e50e1aa958252e5f81750088e2e2d236a19d9
Aakash Yadav <aakashyadav@Aakashs-MacBook-Air.local>
2024-01-01 16:29:10 +0530

    added socket

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,5 @@
 const { io } = require("socket.io-client");
 
-export const socket = io('http://localhost:4000')
+const url = process.env.NODE_ENV === 'production' ? 'https://sketch-app-backend-dimc.onrender.com' : 'http://localhost:4000'
+
+export const socket = io(url)
